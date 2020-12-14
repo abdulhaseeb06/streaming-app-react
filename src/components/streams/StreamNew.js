344e881fccb4a143fe1a0edfaefc3b968654333a
Abdul Haseeb <abdulhaseeb2931@gmail.com>
2020-12-15 00:01:34 +0500

    Redux Form Setup

--- src/components/streams/StreamNew.js
+++ src/components/streams/StreamNew.js
@@ -1,5 +1,32 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
+const StreamNew = (props) => {
+  const renderInput = ({ input, label }) => {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  };
 
-export default function StreamNew() {
-  return <div>Stream New</div>;
-}
+  const onSubmit = (formValues) => {
+    console.log(formValues);
+  };
+
+  return (
+    <form className="ui form" onSubmit={props.handleSubmit(onSubmit)}>
+      <Field name="title" component={renderInput} label="Enter title" />
+      <Field
+        name="description"
+        component={renderInput}
+        label="Enter descripiton"
+      />
+      <button className="ui button primary">Submit</button>
+    </form>
+  );
+};
+
+export default reduxForm({
+  form: "streamCreate",
+})(StreamNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import { AuthReducer } from "./authReducer";
-
+import { reducer as formReducer } from "redux-form";
 export default combineReducers({
   auth: AuthReducer,
+  form: formReducer,
 });
